b597746a889455fbecf9bab3677b416e8e20a016
rmilbradt <rmilbradt@gmail.com>
2018-04-17 17:18:01 -0300

    push inicial do enunciado e classe do modelo comum.

--- src/br/ufsm/csi/seguranca/pila/model/Transacao.java
+++ src/br/ufsm/csi/seguranca/pila/model/Transacao.java
@@ -1,9 +1,38 @@
 package br.ufsm.csi.seguranca.pila.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by cpol on 17/04/2018.
  */
 public class Transacao implements Serializable {
+
+    private String idNovoDono;
+    private Date dataTransacao;
+    private byte[] assinaturaDono;
+
+    public String getIdNovoDono() {
+        return idNovoDono;
+    }
+
+    public void setIdNovoDono(String idNovoDono) {
+        this.idNovoDono = idNovoDono;
+    }
+
+    public Date getDataTransacao() {
+        return dataTransacao;
+    }
+
+    public void setDataTransacao(Date dataTransacao) {
+        this.dataTransacao = dataTransacao;
+    }
+
+    public byte[] getAssinaturaDono() {
+        return assinaturaDono;
+    }
+
+    public void setAssinaturaDono(byte[] assinaturaDono) {
+        this.assinaturaDono = assinaturaDono;
+    }
 }
